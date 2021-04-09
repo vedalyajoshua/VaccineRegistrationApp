66ab0ed352c926c23a4e0a8e703e69b818a7c02b
trsaund <tms.saunders@gmail.com>
2021-04-09 07:41:06 -0500

    created constructor for Patient class

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,15 @@
+import java.util.Date;
+
 public class Patient {
+    private String patientName, patientZipCode, patientEmail, patientPhone;
+    private Date patientDOB;
+
+    public Patient(String patientName, Date patientDOB, String patientZipCode,
+                   String patientEmail, String patientPhone) {
+        this.patientName = patientName;
+        this.patientDOB = patientDOB;
+        this.patientZipCode = patientZipCode;
+        this.patientEmail = patientEmail;
+        this.patientPhone = patientPhone;
+    }
 }
