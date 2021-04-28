829019205c1a6a7a6a511d8c03222ccdcd2d08a9
vedalyajoshua <82358899+vedalyajoshua@users.noreply.github.com>
2021-04-28 18:09:38 -0500

    Update RegistrationGUI.java
    adding the method eligibilityScreening- this will remove the main panel and replace it with the screening/eligibility panel.

--- src/RegistrationGUI.java
+++ src/RegistrationGUI.java
@@ -8,16 +8,20 @@ public class RegistrationGUI extends JFrame {
     private RegistrationPanel regPanel;
     public RegistrationGUI() {
         super("COVID-19 Vaccine Registration");
-        Height = 400;
-        Width = 600;
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setPreferredSize(new Dimension(Width, Height));
-        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
         regPanel = new RegistrationPanel(this);
         add(regPanel);
+        pack();
         setVisible(true);
     }
-
+    public void eligibilityScreening(){
+        remove(regPanel);
+        JPanel screeningPanel = new Screening();
+        add(screeningPanel);
+        setVisible(true);
+        repaint();    
+    }
+    
     public static void main(String[] args) {
         RegistrationGUI registrationGUI = new RegistrationGUI();
 
